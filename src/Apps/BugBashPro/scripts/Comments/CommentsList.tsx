f2248d404b2f61d7cb97e3db6e06d3d51e8f058b
Mohit Bagra <mbagra@microsoft.com>
2019-03-21 16:18:14 -0400

    comment hook

--- src/Apps/BugBashPro/scripts/Comments/CommentsList.tsx
+++ src/Apps/BugBashPro/scripts/Comments/CommentsList.tsx
@@ -1,47 +1,20 @@
 import "./CommentsList.scss";
 
 import { ago } from "azure-devops-ui/Utilities/Date";
-import { CommentActions, getComments, getCommentsStatus, ICommentsAwareState } from "BugBashPro/Redux/Comments";
 import { IBugBashItemComment } from "BugBashPro/Shared/Contracts";
 import { IdentityView } from "Common/Components/IdentityView";
 import { Loading } from "Common/Components/Loading";
 import { LoadStatus } from "Common/Contracts";
-import { useActionCreators } from "Common/Hooks/useActionCreators";
-import { useMappedState } from "Common/Hooks/useMappedState";
 import * as React from "react";
+import { useComments } from "./Hooks/useComments";
 
 interface ICommentsListProps {
     bugBashItemId: string;
 }
 
-interface ICommentsListStateProps {
-    comments?: IBugBashItemComment[];
-    status: LoadStatus;
-}
-
-const Actions = {
-    requestCommentsLoad: CommentActions.commentsLoadRequested
-};
-
 export function CommentsList(props: ICommentsListProps) {
     const { bugBashItemId } = props;
-    const mapStateToProps = React.useCallback(
-        (state: ICommentsAwareState): ICommentsListStateProps => {
-            return {
-                comments: getComments(state, bugBashItemId),
-                status: getCommentsStatus(state, bugBashItemId)
-            };
-        },
-        [bugBashItemId]
-    );
-    const { comments, status } = useMappedState(mapStateToProps);
-    const { requestCommentsLoad } = useActionCreators(Actions);
-
-    React.useEffect(() => {
-        if (!comments || status === LoadStatus.NotLoaded) {
-            requestCommentsLoad(bugBashItemId);
-        }
-    }, [bugBashItemId]);
+    const { comments, status } = useComments(bugBashItemId);
 
     if (!comments || status === LoadStatus.Loading) {
         return <Loading />;
---
+++ src/Apps/BugBashPro/scripts/Comments/Hooks/useComments.ts
@@ -0,0 +1,37 @@
+import { CommentActions, getComments, getCommentsStatus, ICommentsAwareState } from "BugBashPro/Redux/Comments";
+import { IBugBashItemComment } from "BugBashPro/Shared/Contracts";
+import { LoadStatus } from "Common/Contracts";
+import { useActionCreators } from "Common/Hooks/useActionCreators";
+import { useMappedState } from "Common/Hooks/useMappedState";
+import { useCallback, useEffect } from "react";
+
+export function useComments(bugBashItemId: string): IUseCommentsMappedState {
+    const mapStateToProps = useCallback(
+        (state: ICommentsAwareState): IUseCommentsMappedState => {
+            return {
+                comments: getComments(state, bugBashItemId),
+                status: getCommentsStatus(state, bugBashItemId)
+            };
+        },
+        [bugBashItemId]
+    );
+    const { comments, status } = useMappedState(mapStateToProps);
+    const { requestCommentsLoad } = useActionCreators(Actions);
+
+    useEffect(() => {
+        if (!comments || status === LoadStatus.NotLoaded) {
+            requestCommentsLoad(bugBashItemId);
+        }
+    }, [bugBashItemId]);
+
+    return { comments, status };
+}
+
+interface IUseCommentsMappedState {
+    comments: IBugBashItemComment[] | undefined;
+    status: LoadStatus;
+}
+
+const Actions = {
+    requestCommentsLoad: CommentActions.commentsLoadRequested
+};
