c043d658d4b3873d7b0e4ad559a6f124d4aff2a4
Mohit Bagra <mbagra@microsoft.com>
2019-04-26 00:54:31 -0400

    minor fixes

--- src/Apps/Checklist/scripts/Components/WorkItemFormGroup/ChecklistItem.tsx
+++ src/Apps/Checklist/scripts/Components/WorkItemFormGroup/ChecklistItem.tsx
@@ -28,27 +28,37 @@ export function ChecklistItem(props: IChecklistItemProps) {
     const { deleteChecklistItem, updateChecklistItem } = useActionCreators(Actions);
     const isCompleted = checklistItem.state === ChecklistItemState.Completed;
 
-    const deleteItem = React.useCallback(() => {
-        deleteChecklistItem(workItemId, checklistItem.id);
-    }, [checklistItem.id]);
+    const onItemClick = React.useCallback(() => {
+        updateChecklistItem(workItemId, { ...checklistItem, state: isCompleted ? ChecklistItemState.New : ChecklistItemState.Completed });
+    }, [workItemId, isCompleted, checklistItem]);
 
     const onCheckboxChange = React.useCallback(
-        (_ev: React.FormEvent<HTMLElement | HTMLInputElement>, checked: boolean) => {
+        (e: React.FormEvent<HTMLElement | HTMLInputElement>, checked: boolean) => {
+            e.stopPropagation();
             updateChecklistItem(workItemId, { ...checklistItem, state: checked ? ChecklistItemState.Completed : ChecklistItemState.New });
         },
         [workItemId, checklistItem]
     );
 
-    const onItemClick = React.useCallback(
-        (_ev: React.MouseEvent) => {
-            updateChecklistItem(workItemId, { ...checklistItem, state: isCompleted ? ChecklistItemState.New : ChecklistItemState.Completed });
+    const onDeleteClick = React.useCallback(
+        (e: React.MouseEvent<HTMLElement>) => {
+            e.stopPropagation();
+            deleteChecklistItem(workItemId, checklistItem.id);
+        },
+        [checklistItem.id]
+    );
+
+    const onEditClick = React.useCallback(
+        (e: React.MouseEvent<HTMLElement>) => {
+            e.stopPropagation();
+            console.log("edit");
         },
         [workItemId, isCompleted, checklistItem]
     );
 
     return (
         <div className={css("checklist-item-container scroll-hidden flex-row flex-center", className, isCompleted && "completed")}>
-            <Icon className="drag-handle flex-noshrink" iconName="GlobalNavButton" />
+            <Icon className="drag-handle flex-noshrink" iconName="Cancel" />
             <div className="checklist-item scroll-hidden flex-row flex-center flex-grow" onClick={onItemClick}>
                 <Checkbox className="flex-noshrink" disabled={disabled} checked={isCompleted} onChange={onCheckboxChange} />
                 {checklistItem.required && <div className="required-item">*</div>}
@@ -59,6 +69,7 @@ export function ChecklistItem(props: IChecklistItemProps) {
                     <Button
                         disabled={disabled}
                         subtle={true}
+                        onClick={onEditClick}
                         className="checklist-command-item "
                         iconProps={{ iconName: "Edit" }}
                         tooltipProps={{ text: "Edit" }}
@@ -66,7 +77,7 @@ export function ChecklistItem(props: IChecklistItemProps) {
                     <Button
                         subtle={true}
                         disabled={disabled}
-                        onClick={deleteItem}
+                        onClick={onDeleteClick}
                         className="checklist-command-item error-item"
                         iconProps={{ iconName: "Delete" }}
                         tooltipProps={{ text: "Delete" }}
