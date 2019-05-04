21ec3eabbdd7a1490a464471d2dda97f75232d0d
Mohit Bagra <mbagra@microsoft.com>
2019-05-04 18:25:57 -0400

    context menu for checklist items

--- src/Apps/Checklist/scripts/Components/Shared/ChecklistItem.tsx
+++ src/Apps/Checklist/scripts/Components/Shared/ChecklistItem.tsx
@@ -1,16 +1,12 @@
 import "./ChecklistItem.scss";
 
-import { Button } from "azure-devops-ui/Button";
 import { Checkbox } from "azure-devops-ui/Checkbox";
-import { ContentSize } from "azure-devops-ui/Components/Callout/Callout.Props";
-import * as CustomDialog_Async from "azure-devops-ui/Dialog";
 import { Tooltip } from "azure-devops-ui/TooltipEx";
-import { css, getSafeId } from "azure-devops-ui/Util";
+import { css } from "azure-devops-ui/Util";
 import { AsyncComponent } from "Common/Components/AsyncComponent";
 import { emptyRenderer } from "Common/Components/Renderers";
 import { LoadStatus } from "Common/Contracts";
 import { useActionCreators } from "Common/Hooks/useActionCreators";
-import { confirmAction } from "Common/ServiceWrappers/HostPageLayoutService";
 import * as React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import { ChecklistContext, ChecklistItemStates } from "../../Constants";
@@ -19,7 +15,7 @@ import { useChecklistStatus } from "../../Hooks/useChecklistStatus";
 import { ChecklistItemState, ChecklistType, IChecklistItem } from "../../Interfaces";
 import { ChecklistActions } from "../../Redux/Checklist/Actions";
 import { IBaseProps, IChecklistItemCommonProps } from "../Props";
-import { ChecklistItemEditor } from "./ChecklistItemEditor";
+import * as ChecklistItemContextMenu_Async from "./ChecklistItemContextMenu";
 
 interface IChecklistItemProps extends IBaseProps, IChecklistItemCommonProps {
     checklistItem: IChecklistItem;
@@ -33,15 +29,14 @@ const Actions = {
     updateChecklistItem: ChecklistActions.checklistItemUpdateRequested
 };
 
-const dialogLoader = async () => import("azure-devops-ui/Dialog");
+const contextMenuLoader = async () => import("./ChecklistItemContextMenu");
 
 export function ChecklistItem(props: IChecklistItemProps) {
     const { checklistItem, checklistType, className, canDeleteItem, canEditItem, canUpdateItemState, index, disableDrag } = props;
     const idOrType = React.useContext(ChecklistContext);
-    const { deleteChecklistItem, updateChecklistItem } = useActionCreators(Actions);
+    const { updateChecklistItem } = useActionCreators(Actions);
     const status = useChecklistStatus(idOrType);
     const { wordWrap, hideCompletedItems } = useChecklistSettings();
-    const [editorOpen, setEditorOpen] = React.useState(false);
 
     const isCompleted = checklistItem.state === ChecklistItemState.Completed;
     const disabled = status !== LoadStatus.Ready;
@@ -71,145 +66,76 @@ export function ChecklistItem(props: IChecklistItemProps) {
         [canUpdateItemState, disabled, idOrType, checklistItem, checklistType]
     );
 
-    const onDeleteClick = React.useCallback(
-        (e: React.MouseEvent<HTMLElement>) => {
-            if (!disabled && canDeleteItem) {
-                e.stopPropagation();
-                confirmAction("Are you sure you want to delete this item?", "", (ok: boolean) => {
-                    if (ok) {
-                        deleteChecklistItem(idOrType, checklistItem.id, checklistType);
-                    }
-                });
-            }
-        },
-        [disabled, canDeleteItem, idOrType, checklistItem.id, checklistType]
-    );
-
-    const onEditClick = React.useCallback(
-        (e: React.MouseEvent<HTMLElement>) => {
-            if (!disabled && canEditItem) {
-                e.stopPropagation();
-                setEditorOpen(true);
-            }
-        },
-        [disabled, canEditItem]
-    );
-
-    const onEditorDismiss = React.useCallback(() => {
-        setEditorOpen(false);
-    }, []);
-
     if (hideCompletedItems && isCompleted) {
         return null;
     }
 
     return (
-        <>
-            {editorOpen && (
-                <AsyncComponent loader={dialogLoader} loadingComponent={emptyRenderer}>
-                    {(m: typeof CustomDialog_Async) => (
-                        <m.CustomDialog
-                            onDismiss={onEditorDismiss}
-                            defaultActiveElement={`#${getSafeId("checklist-item-text")}`}
-                            modal={true}
-                            escDismiss={true}
-                            contentSize={ContentSize.Small}
-                            className="item-editor-dialog"
-                        >
-                            <ChecklistItemEditor
-                                checklistType={checklistType}
-                                canUpdateItemState={canUpdateItemState}
-                                checklistItem={checklistItem}
-                                onDismiss={onEditorDismiss}
-                            />
-                        </m.CustomDialog>
+        <Draggable
+            draggableId={`item_${checklistItem.id}`}
+            key={`item_${checklistItem.id}`}
+            type={`checklistItems_${checklistType}`}
+            index={index}
+            isDragDisabled={isDragDisabled}
+        >
+            {(provided, snapshot) => (
+                <div
+                    className={css(
+                        "checklist-item-container scroll-hidden flex-row flex-center",
+                        className,
+                        wordWrap && "wrap-text",
+                        isCompleted && "completed",
+                        snapshot.isDragging && "is-dragging",
+                        isDragDisabled && "drag-disabled"
                     )}
-                </AsyncComponent>
-            )}
-            <Draggable
-                draggableId={`item_${checklistItem.id}`}
-                key={`item_${checklistItem.id}`}
-                type={`checklistItems_${checklistType}`}
-                index={index}
-                isDragDisabled={isDragDisabled}
-            >
-                {(provided, snapshot) => (
-                    <div
-                        className={css(
-                            "checklist-item-container scroll-hidden flex-row flex-center",
-                            className,
-                            wordWrap && "wrap-text",
-                            isCompleted && "completed",
-                            snapshot.isDragging && "is-dragging",
-                            isDragDisabled && "drag-disabled"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                >
+                    {!isDragDisabled && (
+                        <div className="drag-handle flex-noshrink" {...provided.dragHandleProps}>
+                            <div className="gripper" />
+                        </div>
+                    )}
+                    <div className="checklist-item scroll-hidden flex-row flex-center flex-grow">
+                        {canUpdateItemState && (
+                            <Checkbox className="flex-noshrink" disabled={disabled} checked={isCompleted} onChange={onCheckboxChange} />
                         )}
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                    >
-                        {!isDragDisabled && (
-                            <div className="drag-handle flex-noshrink" {...provided.dragHandleProps}>
-                                <div className="gripper" />
-                            </div>
-                        )}
-                        <div
-                            className="checklist-item scroll-hidden flex-row flex-center flex-grow"
-                            onClick={canUpdateItemState ? onItemClick : undefined}
-                        >
-                            {canUpdateItemState && (
-                                <Checkbox className="flex-noshrink" disabled={disabled} checked={isCompleted} onChange={onCheckboxChange} />
+
+                        {checklistItem.required && <div className="required-item flex-noshrink">*</div>}
+
+                        {checklistItem.state &&
+                            checklistItem.state !== ChecklistItemState.Completed &&
+                            checklistItem.state !== ChecklistItemState.New && (
+                                <div className={css("checklist-item-state flex-noshrink", ChecklistItemStates[checklistItem.state].className)}>
+                                    {checklistItem.state}
+                                </div>
                             )}
 
-                            {checklistItem.required && <div className="required-item flex-noshrink">*</div>}
-
-                            {checklistItem.state &&
-                                checklistItem.state !== ChecklistItemState.Completed &&
-                                checklistItem.state !== ChecklistItemState.New && (
-                                    <div className={css("checklist-item-state flex-noshrink", ChecklistItemStates[checklistItem.state].className)}>
-                                        {checklistItem.state}
-                                    </div>
-                                )}
-
-                            <Tooltip overflowOnly={true} text={checklistItem.text}>
-                                <div className={css("checklist-item-text flex-grow", !wordWrap && "text-ellipsis")}>{checklistItem.text}</div>
-                            </Tooltip>
-
-                            <div className="flex-noshrink checklist-commandbar">
-                                {!canEditItem && !canDeleteItem && (
-                                    <Button
-                                        subtle={true}
-                                        className="checklist-command-item"
-                                        iconProps={{ iconName: "Info" }}
-                                        tooltipProps={{
-                                            text:
-                                                "This is a default item. To update or delete it, please go to the settings page by clicking the gear icon above."
-                                        }}
-                                    />
-                                )}
-                                {canEditItem && (
-                                    <Button
-                                        disabled={disabled}
-                                        subtle={true}
-                                        onClick={onEditClick}
-                                        className="checklist-command-item"
-                                        iconProps={{ iconName: "Edit" }}
-                                        tooltipProps={{ text: "Edit" }}
-                                    />
-                                )}
-                                {canDeleteItem && (
-                                    <Button
-                                        subtle={true}
-                                        disabled={disabled}
-                                        onClick={onDeleteClick}
-                                        className="checklist-command-item error-item"
-                                        iconProps={{ iconName: "Delete" }}
-                                        tooltipProps={{ text: "Delete" }}
-                                    />
-                                )}
+                        <Tooltip overflowOnly={true} text={checklistItem.text}>
+                            <div
+                                className={css("checklist-item-text flex-grow", !wordWrap && "text-ellipsis")}
+                                onClick={canUpdateItemState ? onItemClick : undefined}
+                            >
+                                {checklistItem.text}
                             </div>
-                        </div>
+                        </Tooltip>
+
+                        <AsyncComponent loader={contextMenuLoader} loadingComponent={emptyRenderer}>
+                            {(m: typeof ChecklistItemContextMenu_Async) => {
+                                const contextMenuProps = {
+                                    checklistItem,
+                                    checklistType,
+                                    canEditItem,
+                                    canDeleteItem,
+                                    canUpdateItemState,
+                                    className: "flex-noshrink"
+                                };
+                                return <m.ChecklistItemContextMenu {...contextMenuProps} />;
+                            }}
+                        </AsyncComponent>
                     </div>
-                )}
-            </Draggable>
-        </>
+                </div>
+            )}
+        </Draggable>
     );
 }
---
+++ src/Apps/Checklist/scripts/Components/Shared/ChecklistItemContextMenu.tsx
@@ -0,0 +1,115 @@
+import { Button } from "azure-devops-ui/Button";
+import { ContentSize } from "azure-devops-ui/Components/Callout/Callout.Props";
+import { CustomDialog } from "azure-devops-ui/Components/Dialog/CustomDialog";
+import { MoreButton } from "azure-devops-ui/Components/Menu/MoreButton";
+import { css, getSafeId } from "azure-devops-ui/Util";
+import { LoadStatus } from "Common/Contracts";
+import { useActionCreators } from "Common/Hooks/useActionCreators";
+import { confirmAction } from "Common/ServiceWrappers/HostPageLayoutService";
+import * as React from "react";
+import { ChecklistContext } from "../../Constants";
+import { useChecklistStatus } from "../../Hooks/useChecklistStatus";
+import { ChecklistType, IChecklistItem } from "../../Interfaces";
+import { ChecklistActions } from "../../Redux/Checklist/Actions";
+import { IBaseProps, IChecklistItemCommonProps } from "../Props";
+import { ChecklistItemEditor } from "./ChecklistItemEditor";
+
+interface IChecklistItemContextMenuProps extends IBaseProps, IChecklistItemCommonProps {
+    checklistItem: IChecklistItem;
+    checklistType: ChecklistType;
+}
+
+const Actions = {
+    deleteChecklistItem: ChecklistActions.checklistItemDeleteRequested
+};
+
+export function ChecklistItemContextMenu(props: IChecklistItemContextMenuProps) {
+    const { checklistItem, checklistType, className, canDeleteItem, canEditItem, canUpdateItemState } = props;
+    const idOrType = React.useContext(ChecklistContext);
+    const { deleteChecklistItem } = useActionCreators(Actions);
+    const status = useChecklistStatus(idOrType);
+    const [editorOpen, setEditorOpen] = React.useState(false);
+
+    const disabled = status !== LoadStatus.Ready;
+
+    const onEditorDismiss = React.useCallback(() => {
+        setEditorOpen(false);
+    }, []);
+
+    const onDeleteClick = React.useCallback(() => {
+        if (!disabled && canDeleteItem) {
+            confirmAction("Are you sure you want to delete this item?", "", (ok: boolean) => {
+                if (ok) {
+                    deleteChecklistItem(idOrType, checklistItem.id, checklistType);
+                }
+            });
+        }
+    }, [disabled, canDeleteItem, idOrType, checklistItem.id, checklistType]);
+
+    const onEditClick = React.useCallback(() => {
+        if (!disabled && canEditItem) {
+            setEditorOpen(true);
+        }
+    }, [disabled, canEditItem]);
+
+    return (
+        <div className={css("checklist-item-context-menu checklist-commandbar", className)}>
+            <>
+                {editorOpen && (
+                    <CustomDialog
+                        onDismiss={onEditorDismiss}
+                        defaultActiveElement={`#${getSafeId("checklist-item-text")}`}
+                        modal={true}
+                        escDismiss={true}
+                        contentSize={ContentSize.Small}
+                        className="item-editor-dialog"
+                    >
+                        <ChecklistItemEditor
+                            checklistType={checklistType}
+                            canUpdateItemState={canUpdateItemState}
+                            checklistItem={checklistItem}
+                            onDismiss={onEditorDismiss}
+                        />
+                    </CustomDialog>
+                )}
+            </>
+
+            {!canEditItem && !canDeleteItem && (
+                <Button
+                    subtle={true}
+                    className="checklist-command-item"
+                    iconProps={{ iconName: "Info" }}
+                    tooltipProps={{
+                        text: "This is a default item. To update or delete it, please go to the settings page by clicking the gear icon above."
+                    }}
+                />
+            )}
+            {(canEditItem || canDeleteItem) && (
+                <MoreButton
+                    disabled={disabled}
+                    contextualMenuProps={{
+                        menuProps: {
+                            id: "checklist-item-context-menu",
+                            items: [
+                                {
+                                    id: "edit",
+                                    disabled: disabled || !canEditItem,
+                                    onActivate: onEditClick,
+                                    iconProps: { iconName: "Edit", className: "communication-foreground" },
+                                    text: "Edit"
+                                },
+                                {
+                                    id: "delete",
+                                    disabled: disabled || !canDeleteItem,
+                                    onActivate: onDeleteClick,
+                                    iconProps: { iconName: "Delete", className: "error-text" },
+                                    text: "Delete"
+                                }
+                            ]
+                        }
+                    }}
+                />
+            )}
+        </div>
+    );
+}
