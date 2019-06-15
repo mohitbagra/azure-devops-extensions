4c9e74db549aeca351e875485a9639efff17d4a6
Mohit Bagra <mbagra@microsoft.com>
2019-06-15 17:18:25 -0400

    show header

--- src/Apps/RelatedWits/scripts/Components/App.tsx
+++ src/Apps/RelatedWits/scripts/Components/App.tsx
@@ -1,15 +1,41 @@
+import { MessageCard, MessageCardSeverity } from "azure-devops-ui/MessageCard";
+import { WorkItemFormListener } from "Common/AzDev/WorkItemForm/Components/WorkItemFormListener";
 import { ReduxHooksStoreProvider } from "Common/Redux";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { createStore } from "redux-dynamic-modules";
 import { getSagaExtension } from "redux-dynamic-modules-saga";
+import { RelatedWitsContext } from "../Constants";
+import { getRelatedWitsModule } from "../Redux/Module";
 import { RelatedWits } from "./RelatedWits";
 
-const store = createStore({}, [], [getSagaExtension()]);
+function App() {
+    return (
+        <WorkItemFormListener>
+            {(activeWorkItemId: number, isNew: boolean) => {
+                if (isNew) {
+                    return (
+                        <MessageCard className="related-wits-message" severity={MessageCardSeverity.Info}>
+                            Please save the work item to see related work items.
+                        </MessageCard>
+                    );
+                } else {
+                    return (
+                        <RelatedWitsContext.Provider value={activeWorkItemId}>
+                            <RelatedWits />
+                        </RelatedWitsContext.Provider>
+                    );
+                }
+            }}
+        </WorkItemFormListener>
+    );
+}
+
+const store = createStore({}, [], [getSagaExtension()], getRelatedWitsModule());
 
 ReactDOM.render(
     <ReduxHooksStoreProvider value={store}>
-        <RelatedWits />
+        <App />
     </ReduxHooksStoreProvider>,
     document.getElementById("ext-container")
 );
--- src/Apps/RelatedWits/scripts/Components/RelatedWits.tsx
+++ src/Apps/RelatedWits/scripts/Components/RelatedWits.tsx
@@ -1,41 +1,82 @@
 import "./Root.scss";
 
-import { MessageCard, MessageCardSeverity } from "azure-devops-ui/MessageCard";
+import { ConditionalChildren } from "azure-devops-ui/ConditionalChildren";
+import { ObservableValue } from "azure-devops-ui/Core/Observable";
+import { FilterBar } from "azure-devops-ui/FilterBar";
+import { CustomHeader, HeaderTitle, HeaderTitleArea, HeaderTitleRow, TitleSize } from "azure-devops-ui/Header";
+import { HeaderCommandBarWithFilter } from "azure-devops-ui/HeaderCommandBar";
 import { Page } from "azure-devops-ui/Page";
-import { WorkItemFormListener } from "Common/AzDev/WorkItemForm/Components/WorkItemFormListener";
-import { useAutoResize } from "Common/AzDev/WorkItemForm/Hooks/useAutoResize";
-import { DynamicModuleLoader } from "Common/Components/DynamicModuleLoader";
+import { KeywordFilterBarItem } from "azure-devops-ui/TextFilterBarItem";
+import { Filter } from "azure-devops-ui/Utilities/Filter";
+import { LoadStatus } from "Common/Contracts";
+import { useMappedState } from "Common/Hooks/useMappedState";
+import { isNullOrEmpty } from "Common/Utilities/String";
 import * as React from "react";
 import { RelatedWitsContext } from "../Constants";
-import { getRelatedWitsModule } from "../Redux/Module";
-
-function RelatedWitsInternal() {
-    useAutoResize();
-
-    return (
-        <WorkItemFormListener>
-            {(activeWorkItemId: number, isNew: boolean) => (
-                <Page className="related-wits-page">
-                    {isNew && (
-                        <MessageCard className="related-wits-message" severity={MessageCardSeverity.Info}>
-                            Please save the work item to see related work items.
-                        </MessageCard>
-                    )}
-                    {!isNew && (
-                        <RelatedWitsContext.Provider value={activeWorkItemId}>
-                            <div className="page-container">Loaded</div>
-                        </RelatedWitsContext.Provider>
-                    )}
-                </Page>
-            )}
-        </WorkItemFormListener>
-    );
-}
+import { IRelatedWitsAwareState } from "../Redux/Contracts";
+import { getRelatedWits, getRelatedWitsError, getRelatedWitsStatus } from "../Redux/Selectors";
 
 export function RelatedWits() {
+    const workItemId = React.useContext(RelatedWitsContext);
+    const filterRef = React.useRef(
+        new Filter({
+            useApplyMode: false
+        })
+    );
+    const filterToggledRef = React.useRef(new ObservableValue<boolean>(false));
+    const onFilterBarDismissClicked = React.useCallback(() => {
+        filterToggledRef.current.value = !filterToggledRef.current.value;
+    }, []);
+
+    const mapState = React.useCallback(
+        (state: IRelatedWitsAwareState) => {
+            return {
+                workItems: getRelatedWits(state, workItemId),
+                status: getRelatedWitsStatus(state, workItemId),
+                error: getRelatedWitsError(state, workItemId)
+            };
+        },
+        [workItemId]
+    );
+    const { workItems, status, error } = useMappedState(mapState);
+    const isLoading = status === LoadStatus.NotLoaded || status === LoadStatus.Loading || !workItems;
+
     return (
-        <DynamicModuleLoader modules={[getRelatedWitsModule()]} cleanOnUnmount={true}>
-            <RelatedWitsInternal />
-        </DynamicModuleLoader>
+        <Page className="related-wits-page">
+            <CustomHeader className="bolt-header-with-commandbar">
+                <HeaderTitleArea>
+                    <HeaderTitleRow>
+                        <HeaderTitle titleSize={TitleSize.Large}>Related work items</HeaderTitle>
+                    </HeaderTitleRow>
+                </HeaderTitleArea>
+                <HeaderCommandBarWithFilter
+                    filter={filterRef.current}
+                    filterToggled={filterToggledRef.current}
+                    className="related-wits-header"
+                    items={[
+                        {
+                            id: "refresh",
+                            important: true,
+                            isPrimary: true,
+                            text: "Refresh",
+                            disabled: !isNullOrEmpty(error) || isLoading,
+                            iconProps: { iconName: "Refresh" }
+                        },
+                        {
+                            id: "settings",
+                            important: true,
+                            text: "Settings",
+                            disabled: !isNullOrEmpty(error) || isLoading,
+                            iconProps: { iconName: "Settings" }
+                        }
+                    ]}
+                />
+            </CustomHeader>
+            <ConditionalChildren renderChildren={filterToggledRef.current}>
+                <FilterBar filter={filterRef.current} className="related-wits-filter" onDismissClicked={onFilterBarDismissClicked}>
+                    <KeywordFilterBarItem filterItemKey={"keyword"} />
+                </FilterBar>
+            </ConditionalChildren>
+        </Page>
     );
 }
